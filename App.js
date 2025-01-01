6f7b353a0f51022ce3919f434235e1b951700474
Sunil Kumar <96238529+sunil-4123@users.noreply.github.com>
2025-01-01 20:08:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from "./components/Auth";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
